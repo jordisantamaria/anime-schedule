704fcfb1205d033d77884f477ac494ed1cc8f9c3
Jordi Santamaria <jordisantamaria1a@gmail.com>
2026-03-20 09:30:46 +0900

    Search icon button covers full right area with pointer cursor

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -98,7 +98,7 @@ export function SearchBar({ animeList }: { animeList: AnimeEntry[] }) {
               inputRef.current?.focus();
             }
           }}
-          className="absolute right-2 top-1/2 -translate-y-1/2 text-white/60 hover:text-white cursor-pointer"
+          className="absolute right-0 top-0 h-full px-2.5 flex items-center text-white/60 hover:text-white cursor-pointer"
         >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-3.5 w-3.5">
             <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z" clipRule="evenodd" />
